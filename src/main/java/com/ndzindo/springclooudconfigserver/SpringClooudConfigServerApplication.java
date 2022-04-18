259a18f2e9540f646b9cfa76db50946729a4c31e
Nedzad Dzindo <nedzad@ubamarket.com>
2022-04-18 23:50:05 +0200

    Init commit.

---
+++ src/main/java/com/ndzindo/springclooudconfigserver/SpringClooudConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.ndzindo.springclooudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+//@EnableConfigServer
+//@EnableConfigServer
+@EnableConfigServer
+public class SpringClooudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClooudConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ndzindo/springclooudconfigserver/SpringClooudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndzindo.springclooudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClooudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
